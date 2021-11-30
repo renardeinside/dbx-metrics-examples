4284f1e0afe1ef8e7781d7ca43c676cc1ae172a5
renardeinside <polarpersonal@gmail.com>
2021-11-30 22:22:22 +0100

    fix init_scripts

--- scripts/datadog-integration.sh
+++ scripts/datadog-integration.sh
@@ -1,83 +1,57 @@
 #!/bin/bash
 
-# echo "Running on the driver? $DB_IS_DRIVER"
-# echo "Driver ip: $DB_DRIVER_IP"
-
-# Get the cluster name
-CLUSTER_NAME=$DB_CLUSTER_NAME
-
-if [ -z "$CLUSTER_NAME" ]
-then
-      echo "\$CLUSTER_NAME is empty"
-      CLUSTER_NAME="pool"
-else
-      echo "\$CLUSTER_NAME is NOT empty"
-fi
-
-CLUSTER_NAME=$CLUSTER_NAME-$DB_CLUSTER_ID
-
-# Set spark.metrics.namespace
-cat >/databricks/driver/conf/custom-spark-metrics-name-conf.conf <<EOL
-[driver] {
-  spark.metrics.namespace = "${CLUSTER_NAME}"
-  spark.sql.streaming.metricsEnabled = "true"
-}
-EOL
-
-#modify metrics config
-#Leave host.period to 1 second to get all metrics
-sudo sed -i '/^driver.sink.ganglia.class/,+4 s/^/#/g' /databricks/spark/conf/metrics.properties
-sudo bash -c "cat <<EOF >> /databricks/spark/conf/metrics.properties
-*.sink.statsd.class=org.apache.spark.metrics.sink.StatsdSink
-*.sink.statsd.host=127.0.0.1
-*.sink.statsd.port=8125
-*.sink.statsd.prefix=spark_metrics
-*.sink.statsd.host.unit=seconds
-*.sink.statsd.host.period=1
-
-master.source.jvm.class=org.apache.spark.metrics.source.JvmSource
-worker.source.jvm.class=org.apache.spark.metrics.source.JvmSource
-driver.source.jvm.class=org.apache.spark.metrics.source.JvmSource
-executor.source.jvm.class=org.apache.spark.metrics.source.JvmSource
-EOF"
+echo "Running on the driver? $DB_IS_DRIVER"
+echo "Driver ip: $DB_DRIVER_IP"
 
 cat <<EOF >> /tmp/start_datadog.sh
 #!/bin/bash
-sudo apt-get install jq
 
-CLUSTER_NAME=$CLUSTER_NAME
-
-echo $DB_IS_DRIVER
-if [[ $DB_IS_DRIVER = "TRUE" ]]; then
+if [ \$DB_IS_DRIVER ]; then
   echo "On the driver. Installing Datadog ..."
 
-  # install the Datadog agent
-  DD_AGENT_MAJOR_VERSION=6 bash -c "\$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+  # CONFIGURE HOST TAGS FOR DRIVER
+  DD_TAGS="environment:\${DD_ENV}","databricks_cluster_id:\${DB_CLUSTER_ID}","databricks_cluster_name:\${DB_CLUSTER_NAME}","spark_host_ip:\${SPARK_LOCAL_IP}","spark_node:driver"
 
-# USING stashd FOR SPARK METRICS
-echo "#using stashd
-use_dogstatsd: true
-collect_ec2_tags: yes
-tags: ["driver:$CLUSTER_NAME", "sparknodetype:driver", "sparknodeip:$DB_DRIVER_IP", "dbclustername:$CLUSTER_NAME"]
+  # INSTALL THE LATEST DATADOG AGENT 7 ON DRIVER AND WORKER NODES
+  DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=\$DD_API_KEY DD_HOST_TAGS=\$DD_TAGS bash -c "\$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
-dogstatsd_port: 8125" >> /etc/datadog-agent/datadog.yaml
+  # WAIT FOR DATADOG AGENT TO BE INSTALLED
+  while [ -z \$datadoginstalled ]; do
+    if [ -e "/etc/datadog-agent/datadog.yaml" ]; then
+      datadoginstalled=TRUE
+    fi
+    sleep 2
+  done
+  echo "Datadog Agent is installed"
 
-  # RESTARTING AGENT
-  sudo service datadog-agent restart
+  # ENABLE LOGS IN datadog.yaml TO COLLECT DRIVER LOGS
+  echo "logs_enabled: true" >> /etc/datadog-agent/datadog.yaml
 
-else
-  echo "On the Executor. Installing Datadog ..."
+  while [ -z \$gotparams ]; do
+    if [ -e "/tmp/driver-env.sh" ]; then
+      DB_DRIVER_PORT=\$(grep -i "CONF_UI_PORT" /tmp/driver-env.sh | cut -d'=' -f2)
+      gotparams=TRUE
+    fi
+    sleep 2
+  done
 
-  # install the Datadog agent
-  DD_AGENT_MAJOR_VERSION=6 bash -c "\$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+  current=\$(hostname -I | xargs)
 
-# USING stashd FOR SPARK METRICS
-echo "#using stashd
-use_dogstatsd: true
-collect_ec2_tags: yes
-tags: ["executor:$CLUSTER_NAME", "sparknodetype:executors","dbclustername:$CLUSTER_NAME"]
-
-dogstatsd_port: 8125" >> /etc/datadog-agent/datadog.yaml
+  # WRITING SPARK CONFIG FILE
+  echo "init_config:
+instances:
+    - spark_url: http://\$DB_DRIVER_IP:\$DB_DRIVER_PORT
+      spark_cluster_mode: spark_driver_mode
+      cluster_name: \$current
+logs:
+    - type: file
+      path: /databricks/driver/logs/*.log
+      source: spark
+      service: databricks
+      log_processing_rules:
+        - type: multi_line
+          name: new_log_start_with_date
+          pattern: \d{2,4}[\-\/]\d{2,4}[\-\/]\d{2,4}.*" > /etc/datadog-agent/conf.d/spark.yaml
 
   # RESTARTING AGENT
   sudo service datadog-agent restart
@@ -86,5 +60,7 @@ fi
 EOF
 
 # CLEANING UP
-chmod a+x /tmp/start_datadog.sh
-/tmp/start_datadog.sh >> /tmp/datadog_start.log 2>&1 & disown
+if [ \$DB_IS_DRIVER ]; then
+  chmod a+x /tmp/start_datadog.sh
+  /tmp/start_datadog.sh >> /tmp/datadog_start.log 2>&1 & disown
+fi
